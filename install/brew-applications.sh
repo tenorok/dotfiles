053e03efffdd186607f1b756937782e71b4758ae
tenorok <mail@tenorok.ru>
2022-06-19 20:13:26 +0300

    brew: Удалён transmission.

--- install/brew-applications.sh
+++ install/brew-applications.sh
@@ -29,7 +29,6 @@ export APPLICATIONS=(
     yandex
     yandex-disk
     dropbox
-    transmission
     vlc
     appzapper
     imageoptim
--- symlinks/.dotfiles/home.sh
+++ symlinks/.dotfiles/home.sh
@@ -1,3 +1,3 @@
 alias mercury="ssh root@95.213.235.195"
-alias venus="ssh root@157.230.27.150"
 alias earth="ssh root@82.148.19.168"
+alias vpn="ssh root@89.208.142.119"
