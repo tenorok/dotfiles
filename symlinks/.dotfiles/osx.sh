d4e0b3731a9fb5ae1bda24894007f77077dcc704
tenorok <mail@tenorok.ru>
2024-03-20 15:46:24 +0300

    Корректное подключение всех автокомплитов из brew
    Исправление ошибки -bash: __ltrim_colon_completions: command not found

--- symlinks/.dotfiles/osx.sh
+++ symlinks/.dotfiles/osx.sh
@@ -9,7 +9,20 @@ alias updatecask='brew update && brew upgrade brew-cask && brew cleanup && brew 
 # M1
 [ -f /opt/homebrew/bin/brew ] && eval "$(/opt/homebrew/bin/brew shellenv)"
 
-include $(brew --prefix)/etc/bash_completion
+# https://docs.brew.sh/Shell-Completion#configuring-completions-in-bash
+if type brew &>/dev/null
+then
+  HOMEBREW_PREFIX="$(brew --prefix)"
+  if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]
+  then
+    source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+  else
+    for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*
+    do
+      [[ -r "${COMPLETION}" ]] && source "${COMPLETION}"
+    done
+  fi
+fi
 
 # NVM нужно инициализировать до использования NPM
 include $(brew --prefix)/opt/nvm/nvm.sh
