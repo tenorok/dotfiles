43ab28de41d4ab81f3f97102b4a6f46d9c5da0a6
tenorok <mail@tenorok.ru>
2024-09-18 10:38:51 +0300

    Настройки ssh

--- install/ssh-keygen.sh
+++ install/ssh-keygen.sh
@@ -2,9 +2,8 @@ EMAIL=${1:-mail@tenorok.ru}
 KEY_NAME=${2:-id_rsa}
 
 [ -d ~/.ssh ] || mkdir ~/.ssh
-[ -f ~/.ssh/config ] || cp -n ssh/config ~/.ssh/config
-[ -f ~/.ssh/rc ] || cp -n ssh/rc ~/.ssh/rc
-chmod 644 ~/.ssh/config
-ssh-keygen -t rsa -C "$EMAIL" -N "" -f ~/.ssh/$KEY_NAME
+[ -f ~/.ssh/config ] || ln -s ssh/config ~/.ssh/config
+[ -f ~/.ssh/rc ] || ln -s ssh/rc ~/.ssh/rc
+ssh-keygen -t ed25519 -C "$EMAIL" -N "" -f ~/.ssh/$KEY_NAME
 pbcopy < ~/.ssh/$KEY_NAME.pub
 echo -e "\033[0;32mPaste key from your clipboard to https://github.com/settings/ssh"
--- symlinks/.dotfiles/home.sh
+++ symlinks/.dotfiles/home.sh
@@ -1,3 +1,4 @@
 alias mercury="ssh root@95.213.235.195"
 alias earth="ssh root@82.148.19.168"
-alias vpn="ssh root@89.208.142.119"
+alias vpn="ssh root@109.107.174.47"
+alias resistance="ssh root@91.243.71.190"
