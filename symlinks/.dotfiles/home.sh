7299c1e1bb0b5cc778e85800703b498198ec0edf
tenorok <mail@tenorok.ru>
2025-02-02 19:52:46 +0300

    Удалён Obsidian и сервер mercury

--- symlinks/.dotfiles/home.sh
+++ symlinks/.dotfiles/home.sh
@@ -1,4 +1,4 @@
-alias mercury="ssh root@95.213.235.195"
+alias venus="ssh root@77.223.99.105"
 alias earth="ssh root@82.148.19.168"
 alias vpn="ssh root@109.107.174.47"
 alias resistance="ssh root@91.243.71.190"
